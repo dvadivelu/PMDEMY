2742dea82510ec205e2e51466fe868e61a4e6876
dvadivelu <dvadivelu@10.0.0.39>
2017-07-12 08:01:39 -0400

    Initial Commit

---
+++ pmdemy/src/main/java/com/pmdemy/pmdemy/PmdemyApplication.java
@@ -0,0 +1,12 @@
+package com.pmdemy.pmdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdemyApplication.class, args);
+	}
+}
